f0f576c5b33916686205dd48995b1923197365c5
Bret Baker <bretbaker808@gmail.com>
2020-09-19 15:41:06 -0400

    login form working, dashboard component created

--- agora-front-end/src/app/components/login/login.component.ts
+++ agora-front-end/src/app/components/login/login.component.ts
@@ -11,9 +11,26 @@ import {User} from '../../models/User';
 })
 export class LoginComponent implements OnInit {
 
+  public username: string;
+  public password: string;
   constructor(private http: HttpClient, private toggler: ToggleService) { }
 
   ngOnInit(): void {
+  }
+
+  async submitForm() {
+    try{
+      let user = await this.http.post<User>('http://localhost:8080/Agora/user/login', {
+        userName: this.username,
+        password: this.password
+      }
+    ).toPromise();
+
+      sessionStorage.setItem("currentUser", JSON.stringify(user));
+      console.log(JSON.stringify(user));
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   toggle() {
