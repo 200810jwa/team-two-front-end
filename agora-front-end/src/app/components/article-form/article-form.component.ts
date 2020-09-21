8d64b61b09a1b955d0ba00296dc041b8271d393f
Bret Baker <bretbaker808@gmail.com>
2020-09-21 19:12:06 -0400

    added byte array logic to article form

--- agora-front-end/src/app/components/article-form/article-form.component.ts
+++ agora-front-end/src/app/components/article-form/article-form.component.ts
@@ -29,4 +29,44 @@ export class ArticleFormComponent implements OnInit {
     console.log(this.publish);
   }
 
+    // file: null;
+		// imageData: null;
+		// byteArray: [];
+	
+		// fileUpload = document.getElementById("file-upload");
+		// fileUploadBtn = document.getElementById("file-upload-btn");
+		// fileName = document.getElementById("file-name");
+		
+		// fileUploadBtn.addEventListener("click", function() {
+		// 	fileUpload.click();
+		// });
+		
+		// fileUpload.addEventListener("change", function() {
+		// 	console.log(fileUpload.files);
+		// 	processFile();
+		// 	if (fileUpload.value) {
+		// 		fileName.innerHTML = fileUpload.value;
+		// 	} else {
+		// 		fileName.innerHTML = "No file chosen"
+		// 	}
+    // });
+    
+  //   processFile() {
+  //     var fileList = fileUpload.files;
+  //     var fileReader = new FileReader();
+  //     if (fileReader && fileList && fileList.length) {
+  //        fileReader.readAsArrayBuffer(fileList[0]);
+  //        fileReader.onload = function () {
+  //           imageData = fileReader.result;
+  //           console.log(imageData);
+  //           let arr = new Uint8Array(imageData);
+  //           console.log(arr);
+  //           for (let i = 0; i < arr.length; i++) {
+  //             byteArray.push(arr[i]);
+  //           }
+  //           console.log(byteArray);
+  //        };
+  //     }
+  //  }
+
 }
